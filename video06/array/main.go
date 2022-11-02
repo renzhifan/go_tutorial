6b50384cce9a6a2ba20ad85ea2ae9fcee54b3464
renzhifan <renzhifan@haohaozhu.com>
2022-11-02 11:26:54 +0800

    Go语言基础之数组

--- video06/array/main.go
+++ video06/array/main.go
@@ -64,6 +64,35 @@ func main() {
 	y := x
 	y[0][0] = 1000
 	fmt.Println(x)
+
+	//求数组[1, 3, 5, 7, 8]所有元素的和
+	var intArr = [...]int{1, 3, 5, 7, 8}
+	// 方法一
+	sum := 0
+	for i := 0; i < len(intArr); i++ {
+		sum += intArr[i]
+		if i == len(intArr)-1 {
+			fmt.Println(sum)
+		}
+	}
+	// 方法二
+	sum1 := 0
+	for _, value := range intArr {
+		sum1 += value
+	}
+	fmt.Println(sum1)
+
+	//找出数组中和为指定值的两个元素的下标，比如从数组[1, 3, 5, 7, 8]中找出和为8的两个元素的下标分别为(0,3)和(1,2)。
+	res := 8
+	for i := 0; i < len(intArr); i++ {
+		for j := i + 1; j < len(intArr); j++ {
+			fmt.Printf("下标:%d,%d;对应的值：%d,%d\n", i, j, intArr[i], intArr[j])
+			if intArr[i]+intArr[j] == res {
+				fmt.Printf("%d+%d=%d,下标为(%d,%d)\n", intArr[i], intArr[j], intArr[i]+intArr[j], i, j)
+			}
+		}
+	}
+
 }
 
 func f1(a [3][2]int) {
