fc08433b69bdf795c9c8fc1e20300348e2fe9285
renzhifan <renzhifan@haohaozhu.com>
2022-11-08 13:31:50 +0800

    练习

--- video16/main.go
+++ video16/main.go
@@ -32,14 +32,16 @@ func main() {
 	// 创建一个班级变量c1
 	c1 := class{
 		Title:    "火箭101",
-		Students: make([]student, 0, 20),
+		Students: []student{},
 	}
+	fmt.Printf("Students.point=%p\n", c1.Students)
 	// 往班级c1中添加学生
 	for i := 0; i < 10; i++ {
 		// 造十个学生
 		tmpStu := newStudent(i, fmt.Sprintf("stu%02d", i))
 		c1.Students = append(c1.Students, tmpStu)
 	}
+	fmt.Printf("Students.point=%p\n", c1.Students)
 	//fmt.Printf("%#v\n", c1)
 	// JSON序列化：Go语言中的数据 -> JSON格式的字符串
 	data, err := json.Marshal(c1)
@@ -48,14 +50,31 @@ func main() {
 		return
 	}
 	//fmt.Printf("%T\n", data)
-	fmt.Printf("%s\n", data)
+	fmt.Printf("data=%s\n", data)
 	// JSON反序列化：JSON格式的字符串 -> Go语言中的数据
 	//jsonStr := `{"title":"火箭101","Students":[{"ID":0,"Name":"小王子"},{"ID":1,"Name":"娜扎"},{"ID":2,"Name":"stu02"}]}`
-	//var c2 class
-	//err = json.Unmarshal([]byte(jsonStr), &c2)
-	//if err != nil {
-	//	fmt.Println("json unmarshal failed, err:", err)
-	//	return
-	//}
-	//fmt.Printf("%#v\n", c2)
+	var c2 class
+	err = json.Unmarshal(data, &c2)
+	if err != nil {
+		fmt.Println("json unmarshal failed, err:", err)
+		return
+	}
+	fmt.Printf("%#v\n", c2)
+
+	//a := [8]int{0, 1, 2, 3, 4, 5, 6, 7}
+	//fmt.Printf("a=%p\n", &a)
+	//s1 := a[:5]
+	//fmt.Printf("s1=%p\n", &s1)
+	//s2 := a[3:6]
+	//fmt.Printf("s2.point=%p s2.point=%v\n", &s2)
+
+	//s1 := make([]int, 3) //[0 0 0]
+	//s2 := s1             //将s1直接赋值给s2，s1和s2共用一个底层数组
+	//fmt.Printf("s1=%p\n", s1)
+	//fmt.Printf("s2=%p\n", s2)
+	//s2[0] = 100
+	//
+	//fmt.Println(s1) //[100 0 0]
+	//fmt.Println(s2) //[100 0 0]
+
 }
